2ccf19be1a57ae05b7cceece2bff1b6071a2d131
Morad <morad98@gmail.com>
2022-06-07 15:44:06 +0300

    About titles is completed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Home Page</title>
+</svelte:head>
+
 <script>
   import CovidStat from "../components/CovidStat.svelte";
 
@@ -5,6 +9,14 @@
 
   import TableContainer from "../components/TableContainer.svelte";
 </script>
+
+
+<div class="section header">
+  <div class="container">
+      <h1>Covid-19</h1>
+      
+  </div>
+</div>
 
 <CovidStat />
 
